52c6a8ab3da704e9dabbda95e9aae61d16f847eb
Matt Brophy <matt@brophy.org>
2020-07-25 12:32:17 -0400

    Remove id attr

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-once id="cv" class="resume">
+    <div v-once class="resume">
         <div class="resume__header">
             <div class="resume__headshot">
                 <img
