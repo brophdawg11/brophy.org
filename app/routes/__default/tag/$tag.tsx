9dbe5f304fe0d05fc719d82409a7e1475dc150a0
Matt Brophy <matt@brophy.org>
2022-01-05 20:55:02 -0500

    Bring over tags page

---
+++ app/routes/__default/tag/$tag.tsx
@@ -0,0 +1,26 @@
+import { LoaderFunction, useLoaderData } from 'remix';
+import invariant from 'tiny-invariant';
+import PostList from '~/components/PostList';
+import { getPosts } from '~/post-api';
+
+export const loader: LoaderFunction = async ({ params }) => {
+    const { tag } = params;
+    const posts = await getPosts();
+    invariant(typeof tag === 'string', 'No tag provided');
+    return {
+        tag,
+        posts: posts.filter((p) => p.tags.includes(tag)),
+    };
+};
+
+export default function Posts() {
+    const { tag, posts } = useLoaderData();
+    return (
+        <div>
+            <h1 className="tag__title">
+                Posts tagged with <span className="tag__tag">{tag}</span>
+            </h1>
+            <PostList posts={posts} />
+        </div>
+    );
+}
